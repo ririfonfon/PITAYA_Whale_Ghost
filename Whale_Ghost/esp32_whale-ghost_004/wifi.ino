9f42d3f8cee9aa297d191b4cad3214ce00a74c63
richard fontaine <ririfonfon10@gmail.com>
2018-03-02 15:26:01 +0100

    Disable Restart.. infinte retry WIFI

--- Whale_Ghost/esp32_whale-ghost_004/wifi.ino
+++ Whale_Ghost/esp32_whale-ghost_004/wifi.ino
@@ -20,26 +20,16 @@ void wifi_init() {
   IPAddress gateway(192, 168, 0, 1);
   IPAddress subnet(255, 255, 255, 0);
 
-  // Enable wifi and connect
+  // Enable wifi
   WiFi.mode(WIFI_STA);
   WiFi.config(ip, gateway, subnet);
-  
+
+  // Connect wifi
   WiFi.onEvent(wifi_event);
   WiFi.begin(ssid, password);
 
-  // If WIFI is the main channel, wait for connection to succeed or restart
-  /*if (WiFi.waitForConnectResult() != WL_CONNECTED)  {
-     #ifdef DEBUG
-      Serial.print("Not connected.. RESTART!");
-    #endif
-    ESP.restart();
-  }*/
-
   // UDP Receiver
   WUdp.begin(udpPort_node);
-  #if defined(DEBUG)
-    Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), udpPort_node);
-  #endif
 }
 
 void wifi_send(uint8_t* message, int sizeM) {
@@ -79,12 +69,10 @@ void wifi_event(WiFiEvent_t event){
     #ifdef DEBUG     
        switch(event) {
         case SYSTEM_EVENT_STA_CONNECTED:
-          Serial.print("WiFi connected - ");
-          Serial.print("IP address: ");
-          Serial.println(WiFi.localIP());
+          Serial.print("WiFi connected");
           break;
         case SYSTEM_EVENT_STA_GOT_IP:
-          Serial.print("Got IP - ");
+          Serial.print("Wifi Connected with IP: ");
           Serial.println(WiFi.localIP());
           break;
         case SYSTEM_EVENT_STA_LOST_IP:
@@ -98,20 +86,21 @@ void wifi_event(WiFiEvent_t event){
     
     if(event == SYSTEM_EVENT_STA_DISCONNECTED){
       retry += 1;
-      if (retry > maxRetry) {
+      /*if (retry > maxRetry) {
         #ifdef DEBUG 
           Serial.println("Can't connect to WIFI... RESTART ESP !");
         #endif 
         WiFi.disconnect(true);
-        delay(100);
+        delay(500);
         ESP.restart();
-      }
+      }*/
       
       #ifdef DEBUG  
         Serial.print(" reconnecting ");
         Serial.print(retry);
-        Serial.print("/");
-        Serial.println(maxRetry);
+        //Serial.print("/");
+        //Serial.print(maxRetry);
+        Serial.println();
       #endif 
       WiFi.reconnect();
     }
