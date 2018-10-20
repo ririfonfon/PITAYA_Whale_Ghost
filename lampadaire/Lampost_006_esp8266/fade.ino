01e4fa78aadfb6e9cff1b3eb1a3adf605fc12f6d
RIRI <ririfonfon10@gmail.com>
2018-10-20 11:31:30 +0200

    cmd != *

--- lampadaire/Lampost_006_esp8266/fade.ino
+++ lampadaire/Lampost_006_esp8266/fade.ino
@@ -1,5 +1,8 @@
 void fade_white() {
-  if (cmd == 1) {
+#ifdef DEBUGDMX
+  Serial.println("fade_white()");
+#endif
+  if (cmd != 2) {
     sendCommand(CMD_FOLDER_CYCLE, 0x0201);
     delay(10);
     cmd = 2;
@@ -13,9 +16,9 @@ void fade_white() {
   Serial.print("BlueNow: ");
   Serial.println(BlueNow);
 #endif
-  RedNow = RedNow +  0.1;
-  GreenNow = GreenNow +  0.1;
-  BlueNow = BlueNow +  0.1;
+  RedNow = RedNow +  0.5;
+  GreenNow = GreenNow +  0.5;
+  BlueNow = BlueNow +  0.5;
   if (RedNow > 255) RedNow = 255;
   if (GreenNow > 255) GreenNow = 255;
   if (BlueNow > 240) BlueNow = 240;
@@ -42,15 +45,48 @@ void fade_white() {
   i = 0;
 }
 
+void fade_red() {
+#ifdef DEBUGDMX
+  Serial.println("fade_red()");
+#endif
+if (cmd != 4) {
+    sendCommand(CMD_FOLDER_CYCLE, 0x0401);
+    delay(10);
+    cmd = 4;
+  }
+  
+  if (touch2 > kRedNow)  kRedNow = kRedNow +  1;
+  if (touch2 < kRedNow)  kRedNow = kRedNow -  1;
+  kGreenNow = 0;
+  kBlueNow = 0;
+  if (kRedNow == touch2 ) kRedNow = touch2;
+
+  dmxbuffer[1] = (kRedNow * kRedNow) / 255;
+  dmxbuffer[2] = (kGreenNow * kGreenNow) / 255;
+  dmxbuffer[3] = (kBlueNow * kBlueNow) / 255;
+  copyDMXToOutput();
+  delay(temp);
+  k = 0;
+}
+
 void fade_pink() {
 #ifdef DEBUGDMX
   Serial.println("fade_pink()");
 #endif
-  jRedNow = jRedNow +  1;
+
+if (cmd != 5) {
+    sendCommand(CMD_FOLDER_CYCLE, 0x0501);
+    delay(10);
+    cmd = 5;
+  }
+  
+  if (touch1 > jRedNow)  jRedNow = jRedNow +  1;
+  if (touch1 < jRedNow)  jRedNow = jRedNow -  1;
   jGreenNow = 0;
-  jBlueNow = jBlueNow +  1;
-  if (jRedNow > 255) jRedNow = 255;
-  if (jBlueNow > 100) jBlueNow = 100;
+  if (touch1 > jBlueNow)  jBlueNow = jBlueNow +  1;
+  if (touch1 < jBlueNow)  jBlueNow = jBlueNow -  1;
+  if (jRedNow == touch1 ) jRedNow = touch1;
+  if (jBlueNow == touch1 ) jBlueNow = touch1;
   dmxbuffer[1] = (jRedNow * jRedNow) / 255;
   dmxbuffer[2] = (jGreenNow * jGreenNow) / 255;
   dmxbuffer[3] = (jBlueNow * jBlueNow) / 255;
@@ -59,29 +95,20 @@ void fade_pink() {
   j = 0;
 }
 
-void fade_lavender() {
+void fade_pink_red() {
 #ifdef DEBUGDMX
-  Serial.println("fade_lavender()");
+  Serial.println("fade_pink_red()");
 #endif
-  kRedNow = kRedNow +  1;
-  kGreenNow = 0;
-  kBlueNow = kBlueNow +  1;
-  if (kRedNow > 100) kRedNow = 100;
-  if (kBlueNow > 255) kBlueNow = 255;
-  dmxbuffer[1] = (kRedNow * kRedNow) / 255;
-  dmxbuffer[2] = (kGreenNow * kGreenNow) / 255;
-  dmxbuffer[3] = (kBlueNow * kBlueNow) / 255;
-  copyDMXToOutput();
-  delay(temp);
-  k = 0;
-}
-void fade_pink_lavender() {
+
+if (cmd != 6) {
+    sendCommand(CMD_FOLDER_CYCLE, 0x0601);
+    delay(10);
+    cmd = 6;
+  }
+  
+  if (pink_red == 0) {
 #ifdef DEBUGDMX
-  Serial.println("fade_pink_lavender()");
-#endif
-  if (pink_lav == 0) {
-#ifdef DEBUGDMX
-    Serial.println("pink_lav = 0");
+    Serial.println("pink_red = 0");
 #endif
 #ifdef DEBUGDMXvalue
     Serial.print("lRedNow: ");
@@ -91,18 +118,18 @@ void fade_pink_lavender() {
     Serial.print("lBlueNow: ");
     Serial.println(lBlueNow);
 #endif
-    lRedNow = lRedNow +  1;
+    if (touch2 > lRedNow)  lRedNow = lRedNow +  1;
+    if (touch2 < lRedNow)  lRedNow = lRedNow -  1;
     lGreenNow = 0;
-    lBlueNow = lBlueNow +  1;
-    if (lRedNow > 255) lRedNow = 255;
-    if (lBlueNow > 200) lBlueNow = 200;
-    if ((lRedNow == 255) && (lBlueNow == 200)) {
-      pink_lav = 1;
+    lBlueNow = 0;
+
+    if (lRedNow == touch2) {
+      pink_red = 1;
     }
   }
-  if (pink_lav == 1) {
+  if (pink_red == 1) {
 #ifdef DEBUGDMX
-    Serial.println("pink_lav = 1");
+    Serial.println("pink_red = 1");
 #endif
 #ifdef DEBUGDMXvalue
     Serial.print("lRedNow: ");
@@ -112,22 +139,23 @@ void fade_pink_lavender() {
     Serial.print("lBlueNow: ");
     Serial.println(lBlueNow);
 #endif
-    lRedNow = lRedNow -  1;
-    lGreenNow = 0;
-    lBlueNow = lBlueNow +  1;
-    if (lRedNow < 100) lRedNow = 100;
-    if (lBlueNow > 255) lBlueNow = 255;
-    if ((lRedNow == 100) && (lBlueNow == 255)) {
+    if (touch1 > lRedNow)  lRedNow = lRedNow +  1;
+    if (touch1 < lRedNow)  lRedNow = lRedNow -  1;
+    jGreenNow = 0;
+    if (touch1 > lBlueNow)  lBlueNow = lBlueNow +  1;
+    if (touch1 < lBlueNow)  lBlueNow = lBlueNow -  1;
+
+    if ((lRedNow == touch1) && (lBlueNow == touch1)) {
 #ifdef DEBUGDMX
       Serial.println("((lRedNow == 100) && (lBlueNow == 255))");
 #endif
-      pink_lav = 2;
+      pink_red = 2;
 
     }
   }
-  if (pink_lav == 2) {
+  if (pink_red == 2) {
 #ifdef DEBUGDMX
-    Serial.println("pink_lav = 2");
+    Serial.println("pink_red = 2");
 #endif
 #ifdef DEBUGDMXvalue
     Serial.print("lRedNow: ");
@@ -137,16 +165,17 @@ void fade_pink_lavender() {
     Serial.print("lBlueNow: ");
     Serial.println(lBlueNow);
 #endif
-    lRedNow = lRedNow +  1;
+    if (touch2 > lRedNow)  lRedNow = lRedNow +  1;
+    if (touch2 < lRedNow)  lRedNow = lRedNow -  1;
     lGreenNow = 0;
     lBlueNow = lBlueNow -  1;
     if (lRedNow > 255) lRedNow = 255;
-    if (lBlueNow < 100) lBlueNow = 100;
-    if ((lRedNow == 255) && (lBlueNow == 100)) {
+    if (lBlueNow < 0) lBlueNow = 0;
+    if ((lRedNow == touch2) && (lBlueNow == 0)) {
 #ifdef DEBUGDMX
       Serial.println("((lRedNow == 255) && (lBlueNow == 100))");
 #endif
-      pink_lav = 1;
+      pink_red = 1;
     }
   }
   dmxbuffer[1] = (lRedNow * lRedNow) / 255;
@@ -158,10 +187,10 @@ void fade_pink_lavender() {
 }
 
 void fade_seq() {
-  if (cmd == 2) {
+  if (cmd != 3) {
     sendCommand(CMD_FOLDER_CYCLE, 0x0301);
     delay(10);
-    cmd = 0;
+    cmd = 3;
   }
 #ifdef DEBUGDMX
   Serial.println("fade_seq()");
@@ -174,9 +203,9 @@ void fade_seq() {
   Serial.print("mBlueNow: ");
   Serial.println(mBlueNow);
 #endif
-  mRedNow = mRedNow - 0.1;
-  mGreenNow = mGreenNow - 0.1;
-  mBlueNow = mBlueNow - 0.1;
+  mRedNow = mRedNow - 0.5;
+  mGreenNow = mGreenNow - 0.5;
+  mBlueNow = mBlueNow - 0.5;
   if (mRedNow < 80) mRedNow = 80;
   if (mGreenNow < 50) mGreenNow = 50;
   if (mBlueNow < 0) mBlueNow = 0;
