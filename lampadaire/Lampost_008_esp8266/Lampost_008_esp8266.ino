32949c4dbed1cc05ff0d10ae833dd847f444dd4e
RIRI <ririfonfon10@gmail.com>
2018-10-20 16:42:07 +0200

    -

--- lampadaire/Lampost_008_esp8266/Lampost_008_esp8266.ino
+++ lampadaire/Lampost_008_esp8266/Lampost_008_esp8266.ino
@@ -102,7 +102,6 @@ uint8_t k;                          //time_loop red  touch2
 uint8_t l;                          //time_loop pink_red touch1 && touch2
 uint8_t m;                          //time_loop i2c
 
-
 uint8_t n;                          //I2C varible de myWire.requestFrom(4, 2);
 
 uint8_t pink_red = 0;               //statue de fade pink red
