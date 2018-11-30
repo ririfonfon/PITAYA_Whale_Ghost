a7d621bc63db8626eea34e1eb59345b78d17b5f2
RIRI <ririfonfon10@gmail.com>
2018-11-30 18:24:51 +0100

    integration du carre dans le send dmx

--- lampadaire/Lampost_010_esp8266/dmx.ino
+++ lampadaire/Lampost_010_esp8266/dmx.ino
@@ -1,18 +1,18 @@
 void copyDMXToOutput(void) {
-  #ifdef DEBUGDMXvalue
-    Serial.print("Red : ");
-    Serial.println(dmxbuffer[1]);
-    Serial.print("Green: ");
-    Serial.println(dmxbuffer[2]);
-    Serial.print("Blue: ");
-    Serial.println(dmxbuffer[3]);
+#ifdef DEBUGDMXvalue
+  Serial.print("Red : ");
+  Serial.println(dmxbuffer[1]);
+  Serial.print("Green: ");
+  Serial.println(dmxbuffer[2]);
+  Serial.print("Blue: ");
+  Serial.println(dmxbuffer[3]);
 #endif
   dmx_red = dmxbuffer[1];
   dmx_green = dmxbuffer[2];
   dmx_blue = dmxbuffer[3];
 
   for (int i = 1; i < DMX_MAX_FRAME; i++) {
-    ESP8266DMX.setSlot(i , dmxbuffer[i]);
+    ESP8266DMX.setSlot(i , (uint8_t((dmxbuffer[i]*dmxbuffer[i]) / 255)));
   }
 
 }
