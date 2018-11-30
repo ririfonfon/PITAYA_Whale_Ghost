7b4a2c8c1b44ea9cc617af4a5205e248bb1d146f
RIRI <ririfonfon10@gmail.com>
2018-11-30 18:26:10 +0100

    check r g b

--- lampadaire/Lampost_010_esp8266/Seq_Loop.ino
+++ lampadaire/Lampost_010_esp8266/Seq_Loop.ino
@@ -6,8 +6,11 @@ int R_lo = 0;
 int G_lo = 0;
 int B_lo = 0;
 
-int R_hi = 200;
-int G_hi = 150;
+//int R_hi = 200;
+//int G_hi = 150;
+
+int R_hi = 255;
+int G_hi = 255;
 
 int R_m1 = 70;
 int G_m1 = 40;
@@ -16,28 +19,28 @@ int R_m2 = 180;
 int G_m2 = 130;
 
 
-int RedList1[]   = {R_m1, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi,
-                    R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_m2, R_hi, R_hi,
+int RedList1[]   = {R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi,
                     R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi,
-                    R_m1, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi,
-                    R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_m2, R_hi, R_hi,
-                    R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_m1, R_hi, R_m1, R_hi
+                    R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi,
+                    R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi,
+                    R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi,
+                    R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi, R_hi
                    };
 
-int GreenList1[] = {G_m1, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
-                   G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_m2, G_hi, G_hi,
-                   G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
-                   G_m1, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
-                   G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_m2, G_hi, G_hi,
-                   G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_m1, G_hi, G_m1, G_hi
+int GreenList1[] = {lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo
                    };
 
-int BlueList1[]  = {B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo,
-                    B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo,
-                    B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo,
-                    B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo,
-                    B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo,
-                    B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo
+int BlueList1[]  = {lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo
                    };
 
 int RedList2[]   = {lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
@@ -48,20 +51,20 @@ int RedList2[]   = {lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
                     lo, lo, lo, lo, lo, lo, lo, lo, lo, lo
                    };
 
-int GreenList2[] = {lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
-                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
-                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
-                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
-                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
-                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_m1, G_hi, G_m1, G_hi
+int GreenList2[] = {G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
+                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
+                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
+                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
+                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
+                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi
                    };
 
-int BlueList2[]  = {B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo,
-                    B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo,
-                    B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo,
-                    B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo,
-                    B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo,
-                    B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo, B_lo
+int BlueList2[]  = {lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
+                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo
                    };
 
 int RedList3[]   = {lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
@@ -80,10 +83,10 @@ int GreenList3[] = {lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
                     lo, lo, lo, lo, lo, lo, lo, lo, lo, lo
                    };
 
-int BlueList3[]  = {lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
-                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
-                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
-                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
-                    lo, lo, lo, lo, lo, lo, lo, lo, lo, lo,
-                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_m1, G_hi, G_m1, G_hi
+int BlueList3[]  = {G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
+                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
+                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
+                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
+                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi,
+                    G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi, G_hi
                    };
