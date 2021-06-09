e923c5a3e246560a0f017fd764682f1b8ff47739
richard fontaine <ririfonfon10@gmail.com>
2021-06-09 14:30:36 +0200

    Create led.h

---
+++ pitaya_5_pwm/src/led.h
@@ -0,0 +1,23 @@
+#ifndef led_h
+#define led_h
+#endif
+
+struct Led
+{
+  // state variables
+  uint8_t pin;
+  bool on;
+
+  // methods
+  void update()
+  {
+    digitalWrite(pin, on ? HIGH : LOW);
+  }
+};
+
+Led onboard_led = {2, false};
+
+void init_led()
+{
+  pinMode(onboard_led.pin, OUTPUT);
+} //init_led
